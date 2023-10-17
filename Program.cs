ca72e19e99063891b5c441e103fd4b12d2f5fec3
Oleg <schika145@gmail.com>
2023-10-17 09:41:46 +0500

    Конец набора

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход число и определяет является ли число чётным
+Console.WriteLine("Введите число");
+string anamber = Console.ReadLine();
+int namber = Convert.ToInt32(anamber);
+
+a = namber % 2;
+
+if (a == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Ошибка, введите другое число");
+}
